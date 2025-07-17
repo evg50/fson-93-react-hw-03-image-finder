4bec538d0f9a71c20a72ffde647b802ba881d222
evg50 <f1.evgeniy4@gmail.com>
2025-07-17 22:13:27 +0200

    init

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+import Phonebook from './Phonebook/Phonebook';
+import { useState } from 'react';
+export const App = () => {
+  return (
+    <div
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   // fontSize: 40,
+    //   color: '#010101',
+    // }}
+    >
+      вафыа
+    </div>
+  );
+};
